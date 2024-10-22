4f4046c2be96b43ba2f4f0d825226a8db9aa7041
Ndeye Fatou Diop <ndeyefatou.diop@monboreal.ca>
2024-10-22 23:50:16 +0200

    classe carte credit

---
+++ AppPaiements/CarteCredit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class CarteCredit : Paiement
+    {
+        public int NumeroCarte { get; set; }
+
+        public CarteCredit (double montant, string description, int carte) : base (montant, description)
+        {
+            NumeroCarte = carte;
+        }
+        public override void AfficherDetails()
+        {
+            Console.WriteLine(); 
+              base.AfficherDetails();
+        }
+
+    }
+}
--- AppPaiements/Program.cs
+++ AppPaiements/Program.cs
@@ -1,7 +1,17 @@
-﻿internal class Program
+﻿using AppPaiements;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        
+        CarteCredit carte_n_1 = new CarteCredit(150, "Paiement en ligne", 123456789);
+        CarteCredit carte_n_2 = new CarteCredit(200, "Paiement resto", 987654321);
+        CarteCredit carte_n_3 = new CarteCredit(300, "Paiement vaisselles", 456789123);
+
+        carte_n_1.AfficherDetails();
+        carte_n_2.AfficherDetails();
+        carte_n_3.AfficherDetails();
+
     }
 }
