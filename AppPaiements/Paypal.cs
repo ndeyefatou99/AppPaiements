b6db32cce42b626aed88c0e4a3f16ee712c7aa5d
Ndeye Fatou Diop <ndeyefatou.diop@monboreal.ca>
2024-10-25 02:19:03 +0200

    classe Paypal

---
+++ AppPaiements/Paypal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class Paypal: Paiement
+    {
+        public string Courriel { get; set; }
+        public Paypal(double montant, string description, string email) : base (montant, description)
+        {
+            Courriel = email;
+        }
+        public override void AfficherDetails()
+        {
+            Console.WriteLine();
+            base.AfficherDetails();
+        }
+
+    }
+}
--- AppPaiements/Program.cs
+++ AppPaiements/Program.cs
@@ -9,9 +9,17 @@ internal class Program
         CarteCredit carte_n_2 = new CarteCredit(200, "Paiement resto", 987654321);
         CarteCredit carte_n_3 = new CarteCredit(300, "Paiement vaisselles", 456789123);
 
+        //instance pour la classe paypal
+
+        Paypal payer1 = new Paypal(15000, "Frais Boreal", "fatoudiopcodou@gmail.com");
+        Paypal payer2 = new Paypal(900, "Payer loyer", "ndeyefatou.diop@monboreal.ca");
+
         carte_n_1.AfficherDetails();
         carte_n_2.AfficherDetails();
         carte_n_3.AfficherDetails();
+
+        payer1.AfficherDetails();
+        payer2.AfficherDetails();
 
     }
 }
