cada84302dd0f2d61decebb650440947250920ab
Ndeye Fatou Diop <ndeyefatou.diop@monboreal.ca>
2024-10-25 02:41:51 +0200

    Classe Utilisateur

---
+++ AppPaiements/Utilisateur.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class Utilisateur
+    {
+        public string Nom {  get; set; }
+        public List <Paiement> Paiements { get; set; }
+
+        public Utilisateur (string nom)
+        {
+            Nom = nom;
+            Paiements = new List <Paiement> ();
+        }
+        public void AfficherInfos()
+        {
+            Console.WriteLine($"Nom : {Nom}, {Paiements} ");
+        }
+        public void AjouterPaiement(Paiement paiement)
+        {
+            Paiements.Add (paiement);
+        }
+    }
+}
